05a6f116b815f2fc4b50df76c91b4084116b175c
John H. Bennett '22 <JHBennett22@chaminet.org>
2020-02-14 20:44:47 -0800

    Bot code for comp

--- src/libIterativeRobot/commands/Auton/JohnsCode.cpp
+++ src/libIterativeRobot/commands/Auton/JohnsCode.cpp
@@ -18,23 +18,21 @@ JohnsCode::JohnsCode() { //*Negitive is forward
   //slipOffRubber->addSequentialCommand(new MoveAnglerTo(0, KMaxMotorSpeed, 1500));
   addSequentialCommand(new MoveArmFor(1100, KMaxMotorSpeed));
   addParallelCommand(new MoveIntakeFor(2300, -KMaxMotorSpeed));
-  addParallelCommand(new MoveAnglerFor(2300, KMaxMotorSpeed));
+  addParallelCommand(new MoveAnglerFor(2500, KMaxMotorSpeed));
   addSequentialCommand(new MoveAnglerFor(2400, -KMaxMotorSpeed));
   addParallelCommand(new MoveIntakeFor(2400, KMaxMotorSpeed));
-  addSequentialCommand(new Delay(400));
   addSequentialCommand(new MoveArmFor(1100, -KMaxMotorSpeed));
   addParallelCommand(new DriveForTime(-KMaxMotorSpeed*0.25, -KMaxMotorSpeed*0.25, 3000));
   //addParallelCommand(slipOffRubber);
-  addParallelCommand(new MoveIntakeFor(3700, KMaxMotorSpeed));
-  addSequentialCommand(new DriveForTime(KMaxMotorSpeed*0.50, KMaxMotorSpeed*0.50, 1000));
-  addSequentialCommand(new DriveForTime(KMaxMotorSpeed*0.25, -KMaxMotorSpeed*0.25, 2000));
-  addSequentialCommand(new DriveForTime(-KMaxMotorSpeed*0.25, -KMaxMotorSpeed*0.25, 1370));
-  addSequentialCommand(new MoveAnglerTo(-2200, 85, 1700));
-  addParallelCommand(new MoveIntakeFor(1000,- 50));
+  addParallelCommand(new MoveIntakeFor(3350, KMaxMotorSpeed));
+  addSequentialCommand(new DriveForTime(KMaxMotorSpeed*0.40, KMaxMotorSpeed*0.40, 950));
+  addSequentialCommand(new DriveForTime(KMaxMotorSpeed*0.25, -KMaxMotorSpeed*0.25, 1500));
+  addSequentialCommand(new DriveForTime(-KMaxMotorSpeed*0.40, -KMaxMotorSpeed*0.40, 1500));
+  addSequentialCommand(new MoveAnglerFor(2300, KMaxMotorSpeed));
   addSequentialCommand(new MoveIntakeFor(1500, -KMaxMotorSpeed));
   addParallelCommand(new Delay(500));
   addSequentialCommand(new MoveIntakeFor(1000, -KMaxMotorSpeed));
-  addSequentialCommand(new DriveForTime(KMaxMotorSpeed, KMaxMotorSpeed, 1000));
+  addSequentialCommand(new DriveForTime(KMaxMotorSpeed, KMaxMotorSpeed, 500));
   
   
 }
